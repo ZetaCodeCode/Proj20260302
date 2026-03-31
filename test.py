3c627eebeb250bc10fde4601f6a081c29092c3e3
ZetaCodeCode <45935428+ZetaCodeCode@users.noreply.github.com>
2026-03-31 14:56:40 +0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,7 @@
+import torch, torch.nn as nn, torch.optim as optim
+import torchvision, torchvision.transforms as transforms
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=False)
